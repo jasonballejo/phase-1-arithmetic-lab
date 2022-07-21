e8f135a6791904f55822dd6182b23a93e8ca9ba9
Jason Ballejo <ballejo19@gmail.com>
2022-07-20 20:50:23 -0400

    commit tree 8fae291d5d683409ba6ab99c756211be3bdd4f1b

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 8
+const num2 = 8
+const multiply = (num1 * num2) - 2
+
+const random = Math.floor(Math.random() * 10) + 1
+
+const num3 = 4
+const num4 = 8
+const mod = num3 % num4
+
+const max = Math.max(4,8,12,20)
